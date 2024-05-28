07c1616127acbabef1b5e1716e5212f3fd7e1da4
qiguangyan <88379858+qiguangyan@users.noreply.github.com>
2024-05-27 20:13:52 -0700

    Update Yan.py

--- Yan.py
+++ Yan.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import zipfile
 import matplotlib.pyplot as plt
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
